042f3d3b141143b31afbbfa4a2beb7798e934050
AleksVlasev <avlasev@gmail.com>
2016-05-20 11:06:17 -0700

    Added some comments to explain how things work + changed the print formatin to include two decimal places

--- assignment.py
+++ assignment.py
@@ -1,5 +1,4 @@
 import requests
-import collections
 import argparse
 from tabulate import tabulate
 
@@ -14,6 +13,7 @@ class APIError(Exception):
 
 
 class NumberOfTransactionsError(Exception):
+	"Error message that comes up if the number of transactions downloaded is different from the stated number."
 	def __init__(self, received_transactions, given_transactions):
 		self.received_transactions = received_transactions
 		self.given_transactions = given_transactions
@@ -31,8 +31,8 @@ indent = '    '
 base_url = "http://resttest.bench.co/transactions/"
 
 
-def page_url(num):
-	return base_url + str(num) + ".json"
+def page_url(number):
+	return base_url + str(number) + ".json"
 
 
 def print_dict(dictionary, indent_str='', inline=False):
@@ -53,6 +53,11 @@ def print_headline(string):
 
 
 def find_duplicates(lis):
+	"""
+	For each item, if we've already seen it, we add it to a list of duplicates. We keep the unique ones in a list too.
+	The complexity of this is O(n^2), where n is the number of entries. For hashable elements, we could have used
+	a dictionary for a large speed up. Given the small number of transactions, I went with a simple solution for now.
+	"""
 	uniques = []
 	duplicates = []
 	for entry in lis:
@@ -64,6 +69,11 @@ def find_duplicates(lis):
 
 
 def categorize_by_key(list_of_dicts, key):
+	"""
+	The list of dictionaries need to consist of dictionaries that have the same sets of keys. The function groups
+	together dictionaries that have the same value for the chosen key. The result is a dictionary, where the keys are
+	the different groups and the values are all the dictionaries that are in the same group.
+	"""
 	categories = {}
 	for dictionary in list_of_dicts:
 		cat = dictionary[key]
@@ -75,7 +85,6 @@ def categorize_by_key(list_of_dicts, key):
 
 
 def decategorize(categories):
-	"If one wishes to undo the categorization."
 	list_of_dicts = []
 	for cat in categories:
 		for dictionary in categories[cat]:
@@ -101,10 +110,21 @@ def get_json_data(url):
 
 
 def get_transactions():
+	"""
+	I thought a lot about how to do this and settled on the following. While there are no errors,
+	we keep downloading the data and moving onto a new page. Once we run out of pages or if there
+	are any other errors, the process stops. Finally, the function checks that we have the same number of
+	transactions as stated in the received data and raises an exception otherwise.
+
+	Possible issue:
+		- What if there are a million pages or more and we start running out of resources?
+		- What if the pages are there, but we get other errors?
+	"""
 	page_num = 0
 	all_ok = True
 	transactions = []
 	given_transactions = 0
+
 	while all_ok:
 		page_num += 1
 		try:
@@ -113,6 +133,7 @@ def get_transactions():
 			transactions.extend(page['transactions'])
 		except APIError:
 			all_ok = False
+
 	received_transactions = len(transactions)
 	if received_transactions != given_transactions:
 		raise NumberOfTransactionsError(received_transactions, given_transactions)
@@ -123,11 +144,23 @@ def get_transactions():
 "Data manipulation functions."
 "--------------------------------------------------------------------------------"
 def clean_company_names(transactions):
-	"TODO: Implement this function :)"
+	"""
+	I realized this function would require some regex most likely and since my experience with this
+	is very limited, I chose to keep it simple for now, and learn how to do it later.
+
+	Another issue here is that I don't know the user. For me, what is garbage may not be what is garbage for the
+	customer. And vice versa. Possible things to remove from the name are: city and province, store numbers,
+	account numbers, usd conversions, company website. It depends on what the customer desires.
+	"""
 	return transactions
 
 
 def balance(transactions, initial_balance=0.0):
+	"""
+	Given a list of transactions, the function tallies up their amounts, with the option for starting with an
+	initial balance. I have assumed it would be enough to work with floats for now. A more robust solution would be
+	to use a class of 'real money' and integer arithmetic to avoid round-off errors. Unless there is interest calculated.
+	"""
 	total = initial_balance
 	for tr in transactions:
 		total += float(tr['Amount'])
@@ -135,6 +168,11 @@ def balance(transactions, initial_balance=0.0):
 
 
 def daily_accumulated_balances(transactions):
+	"""
+	Given a list of transactions, the function sorts them by date, and then computes a running total of the balance,
+	tallying up the balance after each day in a dictionary. It outputs a list of pairs (also list) of dates and
+	daily accumulated balances.
+	"""
 	ordered_transactions = sorted(transactions, key=lambda t: t['Date'])
 	balances = {}
 	runnning_total = 0.
@@ -142,12 +180,19 @@ def daily_accumulated_balances(transactions):
 		runnning_total += float(tran['Amount'])
 		date = tran['Date']
 		balances[date] = runnning_total
-	return to_list(balances)
+	print balances
+	return to_list(balances, sort=True)
 "--------------------------------------------------------------------------------"
 
 if __name__ == '__main__':
 	"App argument parser construction"
 	"--------------------------------------------------------------------------------"
+	"""
+	I decided to run with a command-line approach using flags, where all the arguments can be used in parallel.
+	If we had a proper function to clean up the company names, it is performed first. If the user chooses to
+	only keep unique items, this is done second. Then the rest of the tasks are performed on that cleaned up and
+	possibly-unique data.
+	"""
 	parser = argparse.ArgumentParser(description='Resttest assignment.')
 	parser.add_argument('-c', '--clean',
 						help='clean up the company names',
@@ -172,12 +217,6 @@ if __name__ == '__main__':
 
 	"App logic."
 	"--------------------------------------------------------------------------------"
-	args.duplicates = True
-	args.accumulate = True
-	args.categorized = True
-	args.clean = True
-	args.unique = True
-	args.uncategorized = True
 
 	trans = get_transactions()
 	if args.clean:
@@ -187,7 +226,7 @@ if __name__ == '__main__':
 		uniques, duplicates = find_duplicates(trans)
 		if args.duplicates:
 			print_headline("Duplicate transactions")
-			print tabulate(duplicates, headers='keys')
+			print tabulate(duplicates, headers='keys', floatfmt=".2f")
 		if args.unique:
 			trans = uniques
 
@@ -196,21 +235,21 @@ if __name__ == '__main__':
 	if args.accumulate:
 		print_headline("Daily balances")
 		balances = daily_accumulated_balances(trans)
-		print tabulate(balances, headers=['Date', 'Amount'])
+		print tabulate(balances, headers=['Date', 'Amount'], floatfmt=".2f")
 
 	if args.uncategorized:
 		print_headline("List of transactions")
-		print tabulate(trans, headers='keys')
+		print tabulate(trans, headers='keys', floatfmt=".2f")
 
 	if args.categorized:
 		print_headline("List of transactions by expense category")
 		by_expense = categorize_by_key(trans, key='Ledger')
 		for cat in by_expense:
 			print "\nCategory: {}\nBalance: {}\n".format(cat, balance(by_expense[cat]))
-			print tabulate(by_expense[cat], headers='keys')
+			print tabulate(by_expense[cat], headers='keys', floatfmt=".2f")
 			print ''
 		summarized = [[cat, balance(by_expense[cat])] for cat in by_expense]
 		print_headline("Summary of categorized expenses")
-		print tabulate(summarized, headers=['Ledger', 'Balance'])
+		print tabulate(summarized, headers=['Ledger', 'Balance'], floatfmt=".2f")
 	"--------------------------------------------------------------------------------"
 
